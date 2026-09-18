f641e9a2141213472deeb7ebcf9b3386844eb155
Sheuamodoo <sheuamodoo@gmail.com>
2026-09-18 13:23:04 +0100

    commit

--- Skillsboost Fullstack/JavaScript_Codes/jsIntro_cont.js
+++ Skillsboost Fullstack/JavaScript_Codes/jsIntro_cont.js
@@ -1,3 +1,6 @@
+let eg = document.getElementById("h3");
+eg.textContent = "WELCOME!";
+
 document.getElementById("changed").onmouseover = function () {
   document.getElementById("changed").innerHTML = "This is a changed code";
 };
